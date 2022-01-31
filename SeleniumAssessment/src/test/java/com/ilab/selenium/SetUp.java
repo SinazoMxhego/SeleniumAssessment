77fe6593084175382a5e70a5bc591517627c0185
Sinazo176 <93260826+Sinazo176@users.noreply.github.com>
2022-01-31 23:38:01 +0200

    commit

--- SeleniumAssessment/src/test/java/com/ilab/selenium/SetUp.java
+++ SeleniumAssessment/src/test/java/com/ilab/selenium/SetUp.java
@@ -3,24 +3,36 @@ package com.ilab.selenium;
 import java.time.Duration;
 
 import org.apache.logging.log4j.LogManager;
-
-
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SetUp {
-public static WebDriver driver = null;
-Logger log;
-  @Test
-  public void setup() {
+	public static WebDriver driver = null;
+	Logger log;
+	static ExtentReports extent;
+	static ExtentTest test;
+	@BeforeSuite
+	public void setup() {
 		WebDriverManager.chromedriver().setup();
+
 		driver = new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		//extent report configuration
+
+		extent = new ExtentReports();
+		ExtentSparkReporter spark = new ExtentSparkReporter("./src/test/resources/spark.html");
+		extent.attachReporter(spark);
+
 
 		//log4j2 configuration
 		log= LogManager.getLogger(SetUp.class);
@@ -29,5 +41,8 @@ Logger log;
 			System.err.println("Invalid page title");
 		}
 	}
- 
+	@AfterSuite 
+	public void CleanUp() { 
+		extent.flush();
+	}
 }
